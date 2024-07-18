785927ffc8a8bc747ec004477828e8fa9a3c213b
zeyogh <zeynep.ogh@gmail.com>
2024-07-17 20:22:48 -0500

    download release

--- .github/scripts/collect-jars.sh
+++ .github/scripts/collect-jars.sh
@@ -11,18 +11,8 @@ cd plugins
 
 echo "copying pointmark jar..."
 echo "The GitHub run number is: $GITHUB_RUN_NUMBER"
-echo "check"
-pwd
-ls -la
-echo "check2"
-cd ../../
-pwd
-ls -la
-echo "check3"
-cd ..
-pwd
-ls -la
-cp target/pointmark-1.0.${GITHUB_RUN_NUMBER}.jar ./pointmark-1.0.${GITHUB_RUN_NUMBER}.jar
+cd ../../target
+cp CoordinateExporter-0.1.0-SNAPSHOT.jar ./pointmark-1.0.${GITHUB_RUN_NUMBER}.jar
 
 # https://figshare.com/articles/dataset/Custom_toolbars_and_mini_applications_with_Action_Bar/3397603
 echo "downloading action_bar.jar..."
